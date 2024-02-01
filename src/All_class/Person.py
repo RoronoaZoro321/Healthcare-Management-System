ef15da6570183827cf0ef40628392481778db839
lolipopfounder <87083962+lolipopfounder@users.noreply.github.com>
2024-02-01 23:21:57 +0700

    person

--- src/All_class/Person.py
+++ src/All_class/Person.py
@@ -1,18 +1,44 @@
+import hashlib
+
 class Person:
-    def __init__(self,fname,lname,address, phone_number):
+    def __init__(self, fname: str, lname: str, address: str, 
+                 phone_number: str, photo: str, password: str):
         self.fname = fname
         self.lname = lname
         self.address = address
         self.phone_number = phone_number
+        self.photo = photo
+        self.password = password
+
+    def get_fname(self):
+        return self.fname
+    
+    def get_lname(self):
+        return self.lname
+    
+    def get_address(self):
+        return self.address
+    
+    def get_phone_number(self):
+        return self.phone_number
+    
+    def get_photo(self):
+        return self.photo
+    
+    def get_password(self):
+        return self.password    
 
     def get_detail(self):
-        pass
+        return self.fname, self.lname, self.address, self.phone_number
 
 class Patient(Person):
-    def __init__(self, fname, lname, address, phone_number,patient_id,medical_history):
-        super().__init__(fname, lname, address, phone_number)
+    def __init__(self, fname: str, lname: str, address: str, 
+                 phone_number: str, photo: str, password: str, 
+                 patient_id: int, medical_history: str):
+        super().__init__(fname, lname, address, phone_number, photo, password)
         self.patient_id = patient_id
-        self.update_medical_history = medical_history
+        self.medical_history = {}
+
     def schedule_appointment(self):
         pass
 
@@ -20,7 +46,3 @@ class Patient(Person):
         pass
 
 
-
-
-    
-        
