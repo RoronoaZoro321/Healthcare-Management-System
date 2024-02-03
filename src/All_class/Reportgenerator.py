cc2910bf08f6451adfec9ce6ed248a6c99cd48a8
Jirapat Ruetrakul <65011313@kmitl.ac.th>
2024-02-03 22:24:31 +0700

    update log,reportgenerator and appointment

---
+++ src/All_class/Reportgenerator.py
@@ -0,0 +1,9 @@
+import persistent
+from persistent.list import PersistentList
+
+class ReportGenerator(persistent.Persistent):
+    def generate_patient_report(self):
+        pass
+    
+    def generate_staff_report(self):
+        pass
