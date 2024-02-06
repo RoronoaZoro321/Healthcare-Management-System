a123e779bfe5da1e3b998dd504030497a9136577
JRsssssss <tackelzaza@gmail.com>
2024-02-06 22:12:42 +0700

    fixed appointment

--- src/All_class/Appointment.py
+++ src/All_class/Appointment.py
@@ -1,4 +1,15 @@
-
-
 class Appointment:
-    pass
+    def __init__(self, id, patient, doctor, nurse, confirm, start_time, end_time):
+        self.id = id
+        self.patient = patient
+        self.doctor = doctor
+        self.nurse = nurse
+        self.confirm = confirm
+        self.start_time = start_time
+        self.end_time = end_time
+        
+    def cancle(self):
+        pass
+    
+    def schedule(self):
+        pass
